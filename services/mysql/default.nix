88216e89a9ad4c35d3eaa697a0206c8dc33746d1
Drummss <1509172+Drummss@users.noreply.github.com>
2022-01-02 00:13:00 +0000

    mysql: enable backups and add ed25519 auth plugin

--- services/mysql/default.nix
+++ services/mysql/default.nix
@@ -4,7 +4,10 @@
     enable = true;
     package = pkgs.mariadb;
     dataDir = "/var/lib/mysql";
+    settings.mysqld.plugin-load-add = [ "auth_ed25519.so" ];
   };
 
   services.mysqlBackup.enable = true;
+
+  users.users.mysqlbackup.extraGroups = [ "backups" ];
 }
